7899c3c376eed27fd46bd10d7000583e00ee4590
SamillyBeatriz <samilly.beatriz23@gmail.com>
2024-11-26 15:11:27 -0300

    Adiciona endpoint para upload de imagem

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import { getTodosPosts, criarPost } from "../models/postsModel.js";
 
 export async function listarTodosOsPosts (req, res)
@@ -14,4 +15,23 @@ export async function postarNovoPost(req, res) {
         console.error(erro.message);
         res.status(500).json({'Erro': 'Falha na requisição'});
     }   
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    }
+    try{
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+
+        fs.renameSync(req.file.path, imagemAtualizada);//atualiza o nome do arquivo localmente com id gerado pelo MongoDB
+        res.status(200).json(postCriado);
+    }catch(erro){
+        console.error(erro.message);
+        res.status(500).json({'Erro': 'Falha na requisição'});
+    }   
+    
 }
