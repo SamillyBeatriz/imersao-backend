420635c097c7fb590157d020f0d7f5533bd9c531
SamillyBeatriz <samilly.beatriz23@gmail.com>
2024-11-26 10:30:56 -0300

    Adiciona endpoint para criar um novo post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,7 +1,17 @@
-import { getTodosPosts } from "../models/postsModel.js";
+import { getTodosPosts, criarPost } from "../models/postsModel.js";
 
-export default async function listarTodosOsPosts (req, res)
+export async function listarTodosOsPosts (req, res)
 {
     const posts =  await getTodosPosts();
     res.status(200).json(posts);
+}
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try{
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    }catch(erro){
+        console.error(erro.message);
+        res.status(500).json({'Erro': 'Falha na requisição'});
+    }   
 }
