82ef0c9a1d131015071ea4673694ca47888193d2
SamillyBeatriz <samilly.beatriz23@gmail.com>
2024-11-26 16:05:15 -0300

    Implementa função para atualizar um novo post a partir do ID

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
@@ -5,11 +6,19 @@ const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 export async function getTodosPosts(){
     const db = conexao.db("imersao-instabyte");
     const colecao = db.collection("posts");
-    return colecao.find().toArray();
+    return colecao.find().toArray(); //lista todos os posts registrados do banco
 }
 
 export async function criarPost(novoPost) {
     const db = conexao.db("imersao-instabyte");
     const colecao = db.collection("posts");
-    return colecao.insertOne(novoPost);
+    return colecao.insertOne(novoPost);// cria um novo post dentro do banco
 }
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabyte");
+    const colecao = db.collection("posts");
+    const objectID = ObjectId.createFromHexString(id);//converte o parametro id da função para o formato compreendido pelo MongoDB
+    return colecao.updateOne({_id: new ObjectId(objectID)}, {$set: novoPost})
+}
+
