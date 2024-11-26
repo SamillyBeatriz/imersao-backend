91db3afe451e77d3a7f323def6451c01acf75a53
SamillyBeatriz <samilly.beatriz23@gmail.com>
2024-11-26 15:55:21 -0300

    Adiciona comando para exibir arquivos estáticos para qualquer acesso

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './src/routes/postsRoutes.js';
 
 const app = express(); //inicia o servidor 
+app.use(express.static('uploads'));// exibe arquivos estáticos para todos os acessos
 routes(app)
 
 app.listen(3000, ()=>{
