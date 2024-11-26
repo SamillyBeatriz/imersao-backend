6dfc9f8178dccd18be676854d4f33c2c812aca6f
SamillyBeatriz <samilly.beatriz23@gmail.com>
2024-11-26 15:08:14 -0300

    Configura rota para fazer upload de imagem atráves do multer

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,26 @@
 import express from 'express';
-import {listarTodosOsPosts, postarNovoPost} from '../controllers/postsController.js';
+import multer from 'multer';
+import {listarTodosOsPosts, postarNovoPost, uploadImagem} from '../controllers/postsController.js';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})  //configura o multer com a pasta destino e configuração de arquivos windows
 
 const routes = (app) => {
     app.use(express.json());//conversão de conteúdo para formato json
     //rota para buscar todos os posts
     app.get('/posts', listarTodosOsPosts );
     //rota para criar um novo post
-    app.post('/posts', postarNovoPost ); 
+    app.post('/posts', postarNovoPost );
+    //rota para fazer upload das imagens
+    app.post('/upload', upload.single('Imagem'), uploadImagem )
 }
 
 export default routes;
