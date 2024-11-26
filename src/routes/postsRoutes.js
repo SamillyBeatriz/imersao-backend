2f13674bcc03483614cfd14db852431ee3e906ce
SamillyBeatriz <samilly.beatriz23@gmail.com>
2024-11-26 10:24:21 -0300

    Configura rota para criar um novo post

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import listarTodosOsPosts from '../controllers/postsController.js';
+import {listarTodosOsPosts, postarNovoPost} from '../controllers/postsController.js';
 
 const routes = (app) => {
     app.use(express.json());//conversão de conteúdo para formato json
     //rota para buscar todos os posts
     app.get('/posts', listarTodosOsPosts );
+    //rota para criar um novo post
+    app.post('/posts', postarNovoPost ); 
 }
 
 export default routes;
